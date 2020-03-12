9d0424a0c802e32c5ff187ae0116fced7473eab1
Benny <bennyrobot1143@gmail.com>
2020-03-12 14:22:21 +0800

    changing webpage title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    freeCodeCamp
                 </div>
 
                 <div class="links">
